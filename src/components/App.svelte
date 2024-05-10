f870135cbc4c82127b35a0bd4e6111df2f646b94
Justin Lu <wabasabi359@gmail.com>
2024-05-10 14:30:11 -0700

    Revert "Load temporary data into Svelte"
    This reverts commit 905ef77146824b2cadeb34c3e8942af81fad03f0.

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,27 +1,32 @@
 <script>
+	// original: https://observablehq.com/@d3/choropleth/2
+	// svelte version: Sebastian Lammers https://vis.social/@seblammers
+	import Choropleth from './Chloro.svelte';
+	// import data from './assets/data.csv';
+
   import * as d3 from "d3";
 	import {onMount} from 'svelte';
+  let ldata;
   
   onMount(()=>{
 		load();
+    createGraph();
+    ldata = load()
 	})
 	
 	const load = ()=>{
+    ret = {}
     d3.csv("/data.csv", function(data) {
       // console.log(data);
-      d3.select('body').append('p').text(data['Country Name'])
+      // d3.select('body').append('p').text(data['Country Name'])
+        
       // console.log(data['Country Name'])
+      // tabulate(data, ["Country Name", "Country Code"])
+      return ret.push(data);
     });
+    console.log(ret)
+    return ret
 	}
-
 </script>
 
-<main>
-  <h1>Svelte template</h1>
-
-  <p>Write your HTML here</p>
-</main>
-
-<style>
-  /* Write your CSS here */
-</style>
+<Choropleth {ldata} />
