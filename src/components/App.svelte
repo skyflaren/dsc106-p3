8391355e7af6672dc29a21d3b8a75f969ac529e9
Justin Lu <wabasabi359@gmail.com>
2024-05-14 00:00:55 -0700

    Style text and prompts dynamically, extend chloropleth to borders

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -8,6 +8,9 @@
   let slider_time = 0;
   let world_data = {};
 
+  let innerWidth = 0
+	let innerHeight = 0
+
   // Slider for the Years (2002 - 2022)
   const sliderTimeScale = d3
     .scaleLinear()
@@ -38,8 +41,8 @@
 
 	const load_map = async() => {
     const svg = d3.select("#my_dataviz")
-      .attr("width", 1200)
-      .attr("height", 600)
+      .attr("width", innerWidth)
+      .attr("height", innerHeight-200)
       .call(d3.zoom().on("zoom", (event) => {   
         g.attr("transform", event.transform); 
       }))
@@ -121,42 +124,189 @@ $: {
     load_choropleth(world_data);
   }
 	slider_label = sliderTimeScale(slider_time);
+
+  // d3.select("#my_dataviz")
+  //   .attr("width", innerWidth)
+  //   .attr("height", innerHeight)
+  //   .call(d3.zoom().on("zoom", (event) => {   
+  //     g.attr("transform", event.transform); 
+  //   }));
 }
 </script>
 
+<svelte:window bind:innerWidth bind:innerHeight/>
+
 <main>
-  <h1>World Governance Indicators Choropleth</h1>
-  <label for="governance">Choose a Governance Indicator</label>
-  <select id="governance-select" name="governance">
-    <option value="COC" selected="selected">Control of Corruption</option>
-    <option value="VAA">Voice And Accountability</option>
-    <option value="GE">Government Effectiveness</option>
-    <option value="PI">Political Instability</option>
-    <option value="RQ">Regulatory Quality</option>
-    <option value="ROL">Rule of Law</option>
-  </select>
-  <div class="overlay">	
-		<label>{slider_label}</label>
-		<input
-			id="slider"
-			type="range"
-			min="0"
-			max="20"
-			bind:value={slider_time}
-		/>
-	</div>
-  <div class="choropleth">
+  <div class="top-half">
+    <div>
+      <h1>World Governance Indicators Choropleth</h1>
+      <hr class="top-hr">
+    </div>
+    <div class="row-wrap">
+      <div class="row-elem">
+        <label class="query-label" for="governance"><p class="vert-center">Governance Indicator:</p></label>
+        <span class="query-label"><p class="vert-center">Year:</p></span>
+      </div>
+      <div class="overlay row-elem">
+        <select id="governance-select" name="governance">
+          <option value="COC" selected="selected">Control of Corruption</option>
+          <option value="VAA">Voice And Accountability</option>
+          <option value="GE">Government Effectiveness</option>
+          <option value="PI">Political Instability</option>
+          <option value="RQ">Regulatory Quality</option>
+          <option value="ROL">Rule of Law</option>
+        </select>
+        <div class="row-wrap">
+          <input
+            id="slider"
+            type="range"
+            min="0"
+            max="20"
+            bind:value={slider_time}
+          />
+          <label id="year-label">{slider_label}</label>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="choropleth" width="{innerWidth}">
     <svg id="my_dataviz" width="600" height="600"></svg>
   </div>
 </main>
 
 <style>
+  /* @import 'styles.css'; */
+  @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap');
+
+  :global(body) {
+    margin: 0; padding: 0;
+  }
+
   main {
+    font-family: "Roboto Mono", monospace;
     display: flex;
     flex-direction: column;
     align-items: center;
+    overflow: hidden !important;
+    overflow-y: hidden !important;
   }
+
+  h1{
+    font-size: 2em;
+    color: #161c2e;
+    margin: 30px 15px 0px;
+  }
+
+  .top-half{
+    height: 200px;
+  }
+
+  .top-hr{
+    border: 0.75px #eee solid;
+  }
+
+  #governance-select{
+    font-family: "Roboto Mono", monospace;
+  }
+
+  .query-label{
+    font-size: 0.9em;
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    text-align: right;
+
+    color: #777;
+  }
+
+  .row-wrap {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 0px;
+  }
+
+  .row-elem{
+    padding: 5px 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin-left: auto;
+  }
+
+  .row-elem > *{
+    margin: 5px 0px;
+    height: 35px;
+  }
+
+  .overlay {
+    justify-content: start;
+    margin-left: 0px;
+    margin-right: auto;
+  }
+
+  .overlay > * {
+    background-color: #ddd;
+    padding: 0px 10px;
+    border-radius: 2.5px;
+    border: 1px #bbb solid;
+
+    color: #486b99;
+  }
+
+  .vert-center{
+    margin: 0px;
+  }
+
+  label{
+    font-size: 0.9em;
+    
+  }
+
+  #year-label{
+    padding: 4px 6px;
+    border-radius: 2.5px;
+    background-color: #cecece;
+  }
+
   .choropleth {
-    border: 1px solid black;
+    border-top: 1px solid #bbb;
+    width: 100%;
+  }
+
+  @media (min-width: 200px) {
+    h1 {
+      font-size: 15px;
+    }
+    .query-label{
+      font-size: 9px;
+    }
+    .overlay > * { 
+      padding: 0px 5px;
+      font-size: 10px;
+    }
+  }
+
+  @media (min-width: 500px) {
+    h1 {
+      font-size: 20px;
+    }
+    .query-label{
+      font-size: 15px;
+    }
+  }
+
+  @media (min-width: 800px) {
+    h1 {
+      font-size: 2em;
+    }
+    .query-label{
+      font-size: 0.9em;
+    }
+    .overlay > * { 
+      font-size: 0.7em;
+      padding: 0px 10px;
+    }
   }
 </style>
