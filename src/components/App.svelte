b3c2c85d8ef5b1bf67f9cbd023932f9ea6f3f060
cwang04 <117037369+cwang04@users.noreply.github.com>
2024-05-14 17:12:05 -0700

    play button + legend

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -7,6 +7,14 @@
   let slider_label = "Year";
   let slider_time = 0;
   let world_data = {};
+  const governance_key = {
+    "COC": "Control of Corruption",
+    "VAA" : "Voice and Accountability",
+    "GE": "Government of Effectiveness",
+    "PI": "Political Instability",
+    "RQ": "Regulatory Quality",
+    "ROL": "Rule of Law"
+  }
 
   let innerWidth = 0
 	let innerHeight = 0
@@ -21,24 +29,185 @@
     .scaleLinear()
     .domain([0, 20])
     .rangeRound([2002, 2022]);
-  
-
-  function filterYears(slider_time) {
-    let value = sliderTimeScale(slider_time); //converts slider to Year
-  }
 
   const loadDataAndRenderMap = async() => {
     load_data().then(countryData => {
     load_choropleth(countryData);
     });
 };
+
+  const loadLegend = () => {
+    let svg = d3.select("#legend");
+      svg.selectAll("*").remove();
+
+    legend({
+      color: d3.scaleSequential([-2.5, 2.5], d3.interpolateBlues),
+      title: governance_key[document.getElementById("governance-select").value]
+    })
+
+    function legend({
+  color,
+  title,
+  tickSize = 6,
+  width = 320,
+  height = 44 + tickSize,
+  marginTop = 18,
+  marginRight = 0,
+  marginBottom = 16 + tickSize,
+  marginLeft = 0,
+  ticks = width / 64,
+  tickFormat,
+  tickValues
+} = {}) {
+  const svg = d3.select("#legend")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height])
+    .style("overflow", "visible")
+    .style("display", "block");
+
+  let tickAdjust = g => g.selectAll(".tick line").attr("y1", marginTop + marginBottom - height);
+  let x;
+
+  // Continuous
+  if (color.interpolate) {
+    const n = Math.min(color.domain().length, color.range().length);
+
+    x = color.copy().rangeRound(d3.quantize(d3.interpolate(marginLeft, width - marginRight), n));
+
+    svg.append("image")
+      .attr("x", marginLeft)
+      .attr("y", marginTop)
+      .attr("width", width - marginLeft - marginRight)
+      .attr("height", height - marginTop - marginBottom)
+      .attr("preserveAspectRatio", "none")
+      .attr("xlink:href", ramp(color.copy().domain(d3.quantize(d3.interpolate(0, 1), n))).toDataURL());
+  }
+
+  // Sequential
+  else if (color.interpolator) {
+    x = Object.assign(color.copy()
+      .interpolator(d3.interpolateRound(marginLeft, width - marginRight)), {
+        range() {
+          return [marginLeft, width - marginRight];
+        }
+      });
+
+    svg.append("image")
+      .attr("x", marginLeft)
+      .attr("y", marginTop)
+      .attr("width", width - marginLeft - marginRight)
+      .attr("height", height - marginTop - marginBottom)
+      .attr("preserveAspectRatio", "none")
+      .attr("xlink:href", ramp(color.interpolator()).toDataURL());
+
+    // scaleSequentialQuantile doesn’t implement ticks or tickFormat.
+    if (!x.ticks) {
+      if (tickValues === undefined) {
+        const n = Math.round(ticks + 1);
+        tickValues = d3.range(n).map(i => d3.quantile(color.domain(), i / (n - 1)));
+      }
+      if (typeof tickFormat !== "function") {
+        tickFormat = d3.format(tickFormat === undefined ? ",f" : tickFormat);
+      }
+    }
+  }
+
+  // Threshold
+  else if (color.invertExtent) {
+    const thresholds = color.thresholds ? color.thresholds() // scaleQuantize
+      :
+      color.quantiles ? color.quantiles() // scaleQuantile
+      :
+      color.domain(); // scaleThreshold
+
+    const thresholdFormat = tickFormat === undefined ? d => d :
+      typeof tickFormat === "string" ? d3.format(tickFormat) :
+      tickFormat;
+
+    x = d3.scaleLinear()
+      .domain([-1, color.range().length - 1])
+      .rangeRound([marginLeft, width - marginRight]);
+
+    svg.append("g")
+      .selectAll("rect")
+      .data(color.range())
+      .join("rect")
+      .attr("x", (d, i) => x(i - 1))
+      .attr("y", marginTop)
+      .attr("width", (d, i) => x(i) - x(i - 1))
+      .attr("height", height - marginTop - marginBottom)
+      .attr("fill", d => d);
+
+    tickValues = d3.range(thresholds.length);
+    tickFormat = i => thresholdFormat(thresholds[i], i);
+  }
+
+  // Ordinal
+  else {
+    x = d3.scaleBand()
+      .domain(color.domain())
+      .rangeRound([marginLeft, width - marginRight]);
+
+    svg.append("g")
+      .selectAll("rect")
+      .data(color.domain())
+      .join("rect")
+      .attr("x", x)
+      .attr("y", marginTop)
+      .attr("width", Math.max(0, x.bandwidth() - 1))
+      .attr("height", height - marginTop - marginBottom)
+      .attr("fill", color);
+
+    tickAdjust = () => {};
+  }
+
+  svg.append("g")
+    .attr("transform", `translate(0,${height - marginBottom})`)
+    .call(d3.axisBottom(x)
+      .ticks(ticks, typeof tickFormat === "string" ? tickFormat : undefined)
+      .tickFormat(typeof tickFormat === "function" ? tickFormat : undefined)
+      .tickSize(tickSize)
+      .tickValues(tickValues))
+    .call(tickAdjust)
+    .call(g => g.select(".domain").remove())
+    .call(g => g.append("text")
+      .attr("x", marginLeft)
+      .attr("y", marginTop + marginBottom - height - 6)
+      .attr("fill", "currentColor")
+      .attr("text-anchor", "start")
+      .attr("font-weight", "bold")
+      .text(title));
+
+  return svg.node();
+}
+
+function ramp(color, n = 256) {
+  var canvas = document.createElement('canvas');
+  canvas.width = n;
+  canvas.height = 1;
+  const context = canvas.getContext("2d");
+  for (let i = 0; i < n; ++i) {
+    context.fillStyle = color(i / (n - 1));
+    context.fillRect(i, 0, 1, 1);
+  }
+  return canvas;
+}
+}
+
   
   onMount(()=>{
     load_map();
     loadDataAndRenderMap();
+    loadLegend();
 
     document.getElementById("governance-select").addEventListener("change", () => {
       loadDataAndRenderMap(); 
+      loadLegend();
+    });
+
+    document.getElementById("play").addEventListener("click", () => {
+      cycle_years();
     });
   })
 
@@ -141,12 +310,20 @@
     return filtered_year;
   }
 
+  const cycle_years = async() => {
+    d3.select("#play").attr("pointer-events", "none");
+    for (let i = 0; i < 21; i++) {
+      slider_time = i;
+      await new Promise(r => setTimeout(r, 400));
+    }
+    d3.select("#play").attr("pointer-events", "auto");
+  }
+
 $: {
   if (Object.keys(world_data).length !== 0){
     load_choropleth(world_data);
   }
 	slider_label = sliderTimeScale(slider_time);
-  console.log("uopdate")
 }
 
 const resizeWindow = () => {
@@ -173,6 +350,7 @@ const resizeWindow = () => {
       <div class="row-elem">
         <label class="query-label" for="governance"><p class="vert-center">Governance Indicator:</p></label>
         <span class="query-label"><p class="vert-center">Year:</p></span>
+        <span class="query-label"><p class="vert-center">Legend:</p></span>
       </div>
       <div class="overlay row-elem">
         <select id="governance-select" name="governance">
@@ -184,6 +362,10 @@ const resizeWindow = () => {
           <option value="ROL">Rule of Law</option>
         </select>
         <div class="row-wrap">
+          <svg id="play" width="100" height="100" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="50" cy="50" r="48" stroke="black" stroke-width="4" fill="none" />
+            <polygon points="40,30 70,50 40,70" fill="black" />
+          </svg>
           <input
             id="slider"
             type="range"
@@ -193,6 +375,8 @@ const resizeWindow = () => {
           />
           <label id="year-label">{slider_label}</label>
         </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/d3/6.2.0/d3.min.js"></script>
+        <svg id="legend"></svg>
       </div>
     </div>
   </div>
@@ -260,7 +444,7 @@ const resizeWindow = () => {
   }
 
   .top-half{
-    height: 200px;
+    height: auto;
   }
 
   .top-hr{
@@ -269,6 +453,10 @@ const resizeWindow = () => {
 
   #governance-select{
     font-family: "Roboto Mono", monospace;
+  }
+
+  #slider{
+    min-width: 80%;
   }
 
   .query-label{
